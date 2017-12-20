2087b0f0a490f0f1fec6dfe249f372c7bccccf8a
richardjohns <richard.johns193@gmail.com>
2017-12-20 22:35:26 +1300

    ml modelling working except for 10-fold cross-validation display

--- iris-flowers.py
+++ iris-flowers.py
@@ -60,16 +60,60 @@ pyplot.show()
 # c) Data Transforms
 
 # 4. Evaluate Algorithms
-# a) Split-out validation dataset
+
+# a) Split-out validation dataset - 80% to train our models, 20% for validation
+array = dataset.values
+X = array[:,0:4]
+Y = array[:,4]
+validation_size = 0.20
+seed = 7
+X_train, X_validation, Y_train, Y_validation = train_test_split(X, Y,
+    test_size=validation_size, random_state=seed)
+
 # b) Test options and evaluation metric
+# Use 10-fold cross-validation to estimate accuracy. This will split our dataset into 10 parts, train on 9 and test on 1 and repeat for all combinations of train-test splits. We are using the metric of accuracy to evaluate models.
+
 # c) Spot Check Algorithms
+models = []
+models.append(('LR', LogisticRegression())) 
+models.append(('LDA', LinearDiscriminantAnalysis())) 
+models.append(('KNN', KNeighborsClassifier())) 
+models.append(('CART', DecisionTreeClassifier())) 
+models.append(('NB', GaussianNB())) 
+models.append(('SVM', SVC()))
+# evaluate each model in turn
+results = []
+names = []
+for name, model in models:
+    kfold = KFold(n_splits=10, random_state=seed)
+cv_results = cross_val_score(model, X_train, Y_train, cv=kfold, scoring='accuracy') 
+results.append(cv_results)
+names.append(name)
+msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
+print(msg)
+
 # d) Compare Algorithms
+fig = pyplot.figure() 
+fig.suptitle('Algorithm Comparison') 
+ax = fig.add_subplot(111) 
+pyplot.boxplot(results) 
+ax.set_xticklabels(names) 
+pyplot.show()
 
 # 5. Improve Accuracy
 # a) Algorithm Tuning
 # b) Ensembles
 
 # 6. Finalize Model
+
 # a) Predictions on validation dataset
+knn = KNeighborsClassifier()
+knn.fit(X_train, Y_train)
+predictions = knn.predict(X_validation)
+print(accuracy_score(Y_validation, predictions))
+print(confusion_matrix(Y_validation, predictions))
+print(classification_report(Y_validation, predictions))
+
 # b) Create standalone model on entire training dataset
+
 # c) Save model for later use
